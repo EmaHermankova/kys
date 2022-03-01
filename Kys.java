44023e9a5a5683a1041eb7f670fdf29b8ea4d501
sebyyyy <94870277+EmaHermankova@users.noreply.github.com>
2022-03-01 09:09:22 +0100

    Add files via upload

---
+++ Kys.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kys;
+
+/**
+ *
+ * @author 2021-e-hermankova
+ */
+public class Kys {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
